e9f8d1eb52bead1b224aa915a4799ffd66e40331
Pedro Loureiro <pedro.loureiro@diffusiondata.com>
2023-01-20 12:39:54 +0000

    Added SDK as a parameter for permutation. Added test repetitions and test suite shuffling cleaned up configuration JSON for better readability

--- batch-tester/main.py
+++ batch-tester/main.py
@@ -2,11 +2,24 @@ from __future__ import annotations
 
 import json
 import os
+import random
 import subprocess
 import sys
 import time
 from os.path import abspath, expanduser
 from typing import NamedTuple, Optional
+
+
+class SDK(NamedTuple):
+    name: str
+    path: str
+    command: str
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Parameter(NamedTuple):
@@ -21,44 +34,48 @@ class Parameter(NamedTuple):
 
 
 class Test:
-    parameters: list[Parameter]
+    parameters: dict[str, Parameter]
     environment_variables: list[Parameter]
-    command: str
     result_extraction_properties: dict
     failed_conditions: list[str]
     csv_output: list[str]
-    test_program_path: str
     log_path: str
 
     _results: dict
 
     def __init__(
             self,
-            parameters: list[Parameter],
+            parameters: dict[str, Parameter],
             environment_variables: list[Parameter],
-            command: str,
             result_extraction_properties: dict,
             failed_conditions: list[str],
             csv_output: list[str],
-            test_program_path: str,
             log_path: str
     ):
         self.parameters = parameters
         self.environment_variables = environment_variables
-        self.command = command
         self.result_extraction_properties = result_extraction_properties
         self.failed_conditions = failed_conditions
         self.csv_output = csv_output
-        self.test_program_path = test_program_path
         self.log_path = log_path
         self._results = dict()
 
-    def __generate_parameter_table(self) -> dict:
-        return {p.name: p.possible_values[0] for p in self.parameters}
+    def __get_sdk(self) -> Optional[SDK]:
+        return self.parameters.get("sdk", None).possible_values[0]
 
-    def __generate_command(self):
-        output = self.command
-        for name, value in self.__generate_parameter_table().items():
+    def __flatten_parameters(self) -> dict:
+        return {
+            p.name: p.possible_values[0]
+            for p in self.parameters.values()
+        }
+
+    def __generate_command(self) -> Optional[str]:
+        sdk = self.__get_sdk()
+        if sdk is None:
+            return None
+
+        output = sdk.command
+        for name, value in self.__flatten_parameters().items():
             output = output.replace(f"{{{name}}}", f"{value}")
         return output
 
@@ -97,6 +114,14 @@ class Test:
         return self._results
 
     def run(self) -> Optional[dict]:
+        sdk = self.__get_sdk()
+        if sdk is None:
+            return None
+
+        sdk_path = resolve_path(sdk.path)
+        if sdk_path is None:
+            return None
+
         command = self.__generate_command()
         command_list = command.split(" ")
 
@@ -113,7 +138,7 @@ class Test:
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             env=environment_variables,
-            cwd=self.test_program_path
+            cwd=sdk_path
         )
         # Loop to print and store the output for test result extraction
         result = list()
@@ -132,10 +157,15 @@ class Test:
 
     def get_csv_data(self) -> str:
         output = ""
-        complete_map = self.__generate_parameter_table()
+        complete_map = self.__flatten_parameters()
         complete_map.update({ev.name: ev.possible_values[0] for ev in self.environment_variables})
         complete_map.update(self._results)
-        complete_map["command"] = f"\"{self.__generate_command()}\""
+
+        command = self.__generate_command()
+        if command is None:
+            command = "Invalid"
+
+        complete_map["command"] = command
 
         for output_parameter in self.csv_output:
             output += f"{complete_map.get(output_parameter, None)}, "
@@ -145,11 +175,9 @@ class Test:
         return Test(
             self.parameters.copy(),
             self.environment_variables.copy(),
-            self.command,
             self.result_extraction_properties,
             self.failed_conditions,
             self.csv_output,
-            self.test_program_path,
             self.log_path
         )
 
@@ -169,7 +197,11 @@ def load_test_configuration(file: str) -> dict:
 def build_permutations(test: Test) -> list[Test]:
     permutations = list()
     parameter_to_be_unravelled = next(
-        (p for p in test.parameters if len(p.possible_values) > 1),
+        (
+            key
+            for key, parameter in test.parameters.items()
+            if len(parameter.possible_values) > 1
+        ),
         None
     )
     environment_variable_to_be_unravelled = next(
@@ -183,16 +215,14 @@ def build_permutations(test: Test) -> list[Test]:
 
     elif environment_variable_to_be_unravelled is None:
         # only parameters need to be unravelled
-        for possible_value in parameter_to_be_unravelled.possible_values:
+        for possible_value in test.parameters.get(parameter_to_be_unravelled).possible_values:
             # create a copy of the parameters we received
             test_copy = test.__copy__()
 
-            # remove the parameter to be unravelled
-            test_copy.parameters.remove(parameter_to_be_unravelled)
-
-            # add a copy of it with only one possible value
-            test_copy.parameters.append(
-                Parameter(parameter_to_be_unravelled.name, [possible_value])
+            # replace the parameter to be unravelled with only one possible value
+            test_copy.parameters[parameter_to_be_unravelled] = Parameter(
+                parameter_to_be_unravelled,
+                [possible_value]
             )
 
             # append the new resulting permutations
@@ -250,7 +280,6 @@ def main():
         exit(1)
 
     url = sys.argv[1]
-
     configuration = load_test_configuration("test-configuration.json")
 
     environment_variables = [
@@ -258,17 +287,26 @@ def main():
         for name, values in configuration.get("environment_variables", dict()).items()
     ]
 
-    parameters = [
-        Parameter(name, values)
-        for name, values in configuration.get("parameters", dict()).items()
-    ]
-    # insert url as parameter into the list
-    parameters.append(Parameter("url", [url]))
+    sdks = {
+        name: SDK(name, value.get("path", None), value.get("command", None))
+        for name, value in configuration.get("sdks", dict()).items()
+    }
+
+    parameters = dict()
+    for name, values in configuration.get("parameters", dict()).items():
+        if name == "sdk":
+            replacement_values = [
+                sdks[key] for key in values
+            ]
+            parameter = Parameter(name, replacement_values)
+        else:
+            parameter = Parameter(name, values)
+        parameters[name] = parameter
 
     result_extraction_properties = configuration.get("results", dict())
     failed_conditions = configuration.get("failed_conditions", list())
-    command = configuration.get("command", None)
     delay_between_tests = configuration.get("delay_between_tests_seconds", 5)
+    test_repetitions = configuration.get("test_repetitions", 1)
 
     csv = configuration.get("csv", dict())
     original_csv_path = csv.get("path", None)
@@ -279,46 +317,45 @@ def main():
     original_log_path = log.get("path", None)
     log_path = resolve_path(original_log_path)
 
-    original_test_program_path = configuration.get("test_program_path", None)
-    test_program_path = resolve_path(original_test_program_path)
-
     # verifications
     if csv_path is None:
         print(f"Error: unable to resolve path {original_csv_path}. Aborting")
         exit(1)
-    if test_program_path is None:
-        print(f"Error: unable to resolve path {original_test_program_path}. Aborting")
-        exit(2)
     if log_path is None:
         print(f"Error: unable to resolve path {original_log_path}. Aborting")
-        exit(3)
+        exit(2)
 
     # inject additional parameters
-    parameters.append(Parameter("operating_system", [get_operating_system()]))
+    parameters["url"] = Parameter("url", [url])
+    parameters["operating_system"] = Parameter("operating_system", [get_operating_system()])
 
     test_template = Test(
         parameters,
         environment_variables,
-        command,
         result_extraction_properties,
         failed_conditions,
         csv_sequence,
-        test_program_path,
         log_path
     )
-    test_suite = build_permutations(test_template)
+    test_permutations = build_permutations(test_template)
 
     with open(csv_path, "w+") as f:
-        # write the CSV header
         f.write(f"{test_template.get_csv_header()}{os.linesep}")
 
-        total_permutations = len(test_suite)
-        print(f"Running test suite comprised of {total_permutations} permutations")
+        total_permutations = len(test_permutations)
+        print(f"Running test suite comprised of {total_permutations} permutations and {test_repetitions} repetitions")
+
+        test_suite = list()
+        for i in range(0, test_repetitions):
+            test_suite += test_permutations
+        random.shuffle(test_suite)
+
+        total_tests = len(test_suite)
         for i, test in enumerate(test_suite):
             print(f"{os.linesep}{test}")
-            print(f"Test {i + 1} / {total_permutations} begins{os.linesep}")
+            print(f"Test {i + 1} / {total_tests} begins{os.linesep}")
             test.run()
-            print(f"Test {i + 1} / {total_permutations} done{os.linesep}")
+            print(f"Test {i + 1} / {total_tests} done{os.linesep}")
 
             f.write(f"{test.get_csv_data()}\n")
 
@@ -330,6 +367,5 @@ def main():
     print("Done")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
